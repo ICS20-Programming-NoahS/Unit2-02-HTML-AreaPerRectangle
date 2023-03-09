2984302cd865f2e134dd2705253cde84aec4e610
NoahSmith04 <124379601+NoahSmith04@users.noreply.github.com>
2023-03-09 16:04:23 +0000

    index.html

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// Copyright (c) 2023 by Noah Smith All rights reserved
+//
+// Created By: Noah Smith
+// Created On: March 2023
+// This file contains the JS functions for index.html
+
+function doAreaClicked () {
+  // this function calculates the area of the rectangle
+document.getElementById("multiply-math").innerHTML = "The area of the rectangle is " + (7 * 15)
+}
+
+function doPerimeterClicked () {
+  // this function calculates the perimeter of the rectangle
+document.getElementById("add-math").innerHTML = "The perimeter of the rectangle is " + (7 + 7 + 15 + 15)
+}
